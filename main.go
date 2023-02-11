254bd1af4083e1b722326c5d320d6f49aaed4845
Med-IDBENOUAKRIM <medidbenouakrim@gmail.com>
2023-02-11 11:22:20 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("domain, hasMX, hasSPF, sprRecord, hasDMARC, dmarcRecord")
+
+	for scanner.Scan() {
+		checkDomain(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("Could not read %+v\n", err)
+	}
+}
+
+func checkDomain(domain string) {
+	var hasMX, hasSPF, hasDMARC bool
+	var sprRecord, dmarcRecord string
+
+	mx, err := net.LookupMX(domain)
+
+	if err != nil {
+		log.Fatalf("error: %+v\n", err)
+	}
+
+	if len(mx) > 0 {
+		hasMX = true
+	}
+
+	txt, err := net.LookupTXT(domain)
+
+	if err != nil {
+		log.Printf("error: %+v\n", err)
+	}
+
+	for _, record := range txt {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			sprRecord = record
+			break
+		}
+	}
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+
+	if err != nil {
+		log.Printf("error: %+v\n", err)
+	}
+
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1") {
+			hasDMARC = true
+			dmarcRecord = record
+			break
+		}
+	}
+
+	fmt.Printf("%v, %v, %v, %v, %v, %v", domain, hasMX, hasSPF, sprRecord, hasDMARC, dmarcRecord)
+}
